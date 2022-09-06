437270796be33245abf901d9dba4b42c77ee3d76
chenzuo <chenzuo@hotmail.com>
2022-09-06 03:02:00 -0700

    unit test3 for document approval workflow

---
+++ test/unit/Workflows/UserTaskTest/UserTaskTests/DocumentApprovalWorkflow.cs
@@ -0,0 +1,95 @@
+using Elsa.Activities.Console;
+using Elsa.Activities.ControlFlow;
+using Elsa.Activities.Primitives;
+using Elsa.Builders;
+
+
+namespace UserTaskTests
+{
+    /// <summary>
+    /// A workflow that gives Jack and Lucy a chance to review a submitted document.
+    /// </summary>
+    public class DocumentApprovalWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)
+        {
+
+          var users = new[] { "john", "tom", "jenny" };
+
+            builder
+                .WriteLine("This demonstrates a simple workflow with switch.")
+                .WriteLine("Using switch we can branch a workflow.")
+                .Then<Fork>(fork => fork.WithBranches(users), fork =>
+                {
+                    foreach (var user in users)
+                        fork.When(user)
+                            .WriteLine("A fork created for " + user)
+                            .ThenNamed("Join1");
+                })
+                .Then<Join>(join => join.WithMode(Join.JoinMode.WaitAll)).WithName("Join1")
+                .WriteLine("Workflow finished.");
+
+            // builder
+            //     .WriteLine(context => $"Document received from {context.GetVariable<dynamic>("Document")!.Author.Name}")
+            //     .Then<Fork>(fork1 => fork1.WithBranches("Jack", "Lucy"), fork1 =>
+            //         {
+            //             fork1
+            //                 .When("Jack")
+            //                 .WriteLine(context => $"Jack approve url: \n {context.GenerateSignalUrl("Approve:Jack")}")
+            //                 .Then<Fork>(fork2 => fork2.WithBranches("Approve", "Reject"), fork2 =>
+            //                 {
+            //                     fork2
+            //                         .When("Approve")
+            //                         .SignalReceived("Approve:Jack")
+            //                         .WriteLine("Jack Approved")
+            //                         .SetVariable("Approved", context => context.SetVariable<int>("Approved", approved => approved == 0 ? 1 : approved))
+            //                         .ThenNamed("JoinJack");
+
+            //                     fork2
+            //                         .When("Reject")
+            //                         .SignalReceived("Reject:Jack")
+            //                         .WriteLine("Jack Rejected")
+            //                         .SetVariable<int>("Approved", 2)
+            //                         .ThenNamed("JoinJack");
+            //                 }).WithName("ForkJack")
+            //                 .Add<Join>(x => x.WithMode(Join.JoinMode.WaitAny)).WithName("JoinJack")
+            //                 .ThenNamed("JoinJackLucy");
+
+            //             fork1.When("Lucy")
+            //                 .WriteLine(context => $"Lucy approve url: \n {context.GenerateSignalUrl("Approve:Lucy")}")
+            //                 .Then<Fork>(fork2 => fork2.WithBranches("Approve", "Reject"),
+            //                     fork2 =>
+            //                     {
+            //                         fork2
+            //                             .When("Approve")
+            //                             .SignalReceived("Approve:Lucy")
+            //                             .WriteLine("Lucy Approve")
+            //                             .SetVariable("Approved", context => context.SetVariable<int>("Approved", approved => approved == 0 ? 1 : approved))
+            //                             .ThenNamed("JoinLucy");
+
+            //                         fork2
+            //                             .When("Reject")
+            //                             .SignalReceived("Reject:Lucy")
+            //                             .WriteLine("Lucy Reject")
+            //                             .SetVariable<int>("Approved", 2)
+            //                             .ThenNamed("JoinLucy");
+            //                     }).WithName("ForkLucy")
+            //                 .Add<Join>(x => x.WithMode(Join.JoinMode.WaitAny)).WithName("JoinLucy")
+            //                 .ThenNamed("JoinJackLucy");
+            //         }
+            //     )
+            //     .Add<Join>(x => x.WithMode(Join.JoinMode.WaitAll)).WithName("JoinJackLucy")// wait all Approved or Rejected then to branch JoinJackLucy
+            //     .WriteLine(context => $"Approved: {context.GetVariable<int>("Approved")}").WithName("AfterJoinJackLucy")
+            //     .If(context => context.GetVariable<int>("Approved") == 1, @if =>
+            //     {
+            //         @if
+            //             .When(OutcomeNames.True)
+            //             .WriteLine(context => $"Document ${context.GetVariable<dynamic>("Document")!.Id} approved!`");
+
+            //         @if
+            //             .When(OutcomeNames.False)
+            //             .WriteLine(context => $"Document ${context.GetVariable<dynamic>("Document")!.Id} rejected!`");
+            //     });
+        }
+    }
+}
--- test/unit/Workflows/UserTaskTest/UserTaskTests/UnitTest3.cs
+++ test/unit/Workflows/UserTaskTest/UserTaskTests/UnitTest3.cs
@@ -3,30 +3,34 @@ using System.Threading.Tasks;
 using Elsa.Services;
 using Elsa.Activities.Signaling.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
 
 namespace UserTaskTests
 {
-    public class UnitTest1
+    public class UnitTest3
     {
+        private readonly ITestOutputHelper _output;
+        public UnitTest3(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+
         [Fact]
         public async Task Test1()
         {
             // Create a service container with Elsa services.
             var services = new ServiceCollection()
                 .AddElsa(options => options
-                    .AddConsoleActivities())
+                    .AddConsoleActivities()
+                    .AddWorkflow<DocumentApprovalWorkflow>())
                 .BuildServiceProvider();
-
-            // Run startup actions (not needed when registering Elsa with a Host).
-            var startupRunner = services.GetRequiredService<IStartupRunner>();
-            await startupRunner.StartupAsync();
 
             // Get a workflow runner.
             var workflowRunner = services.GetRequiredService<IBuildsAndStartsWorkflow>();
 
-            // get ISignaler service
-            var signaler = services.GetRequiredService<ISignaler>();
-            Assert.NotNull(signaler);
+            // Run the workflow.
+            await workflowRunner.BuildAndStartWorkflowAsync<DocumentApprovalWorkflow>();
         }
     }
 }
